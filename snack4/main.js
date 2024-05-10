01267eafeb4ef6071e34bc17abf722a2859c2f17
ClaPascarella <claudiopascarella1996@gmail.com>
2024-05-10 10:45:33 +0200

    snack4

---
+++ snack4/main.js
@@ -0,0 +1,10 @@
+const primo = ["a","b","c"];
+const secondo = [1, 2, 3];
+const risultato = [];
+
+for (let i = 0; i < primo.length; i++) {
+    console.log(primo[i]);
+    console.log(secondo[i]);
+}
+
+console.log(risultato);
